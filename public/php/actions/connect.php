81283a7dc39de171c54df3085de4ad61e797ff52
Joris Masson <joris.masson2002@gmail.com>
2023-05-11 11:26:02 +0200

    Rajout du support du nouveau format username de discord

--- public/php/actions/connect.php
+++ public/php/actions/connect.php
@@ -5,8 +5,15 @@ if (!isset($_SESSION["discord_id"]) || !isset($_SESSION["username"]) || !isset($
     } else {
         $user_info = get_user_info(exchange_token(get("code")));
         $_SESSION["discord_id"] = $user_info->id;
-        $_SESSION["username"] = $user_info->username;
-        $_SESSION["discriminator"] = $user_info->discriminator;
+        if (isset($user_info->global_name) && isset($user_info->display_name)) {
+            $_SESSION["use_new_names"] = true;
+            $_SESSION["global_name"] = $user_info->global_name;
+            $_SESSION["display_name"] = $user_info->display_name;
+        } else {
+            $_SESSION["use_new_names"] = false;
+            $_SESSION["username"] = $user_info->username;
+            $_SESSION["discriminator"] = $user_info->discriminator;
+        }
         header("Refresh:0; url=index.php?action=greetings");
         $body = "<h2>Authentification terminée!</h2>";
     }
--- public/php/actions/greetings.php
+++ public/php/actions/greetings.php
@@ -3,9 +3,15 @@ if (!isset($_SESSION["discord_id"]) || !isset($_SESSION["username"]) || !isset($
     $body = "<p>Bonjour, désolé, mais j'ai aucune idée de qui t'es!</p>";
 } else {
     $discord_id = $_SESSION["discord_id"];
-    $username = $_SESSION["username"];
-    $discriminator = $_SESSION["discriminator"];
 
-    $body = "<h2>Oui!</h2>";
-    $body = "Bonjour $username#$discriminator, votre ID discord est $discord_id";
+    $body = "<h2>Hello!</h2>";
+    if ($_SESSION["use_new_names"]) {
+        $global_name = $_SESSION["global_name"];
+        $display_name = $_SESSION["display_name"];
+        $body = "Bonjour $global_name($display_name), votre ID discord est $discord_id";
+    } else {
+        $username = $_SESSION["username"];
+        $discriminator = $_SESSION["discriminator"];
+        $body = "Bonjour $username#$discriminator, votre ID discord est $discord_id";
+    }
 }
