9192c210c12932fc619b21035ee33c8ffd8c542d
Joris Masson <joris.masson2002@gmail.com>
2023-05-17 23:56:02 +0200

    Rajout du nouveau serveur minecraft au server.php

--- public/php/actions/server.php
+++ public/php/actions/server.php
@@ -1,9 +1,16 @@
 <?php
-$minecraft_server_connection = @fsockopen("jo.narukami-edition.fr", "42957", $errno, $errstr, 0.5);
-if ($minecraft_server_connection >= 1) {
-    $minecraft_server_connection = "le serveur est <span style='color: green'>en ligne!</span>";
+$minecraft_vanilla_server_connection = @fsockopen("jo.narukami-edition.fr", "42957", $errno, $errstr, 0.5);
+if ($minecraft_vanilla_server_connection >= 1) {
+    $minecraft_vanilla_server_connection = "le serveur est <span style='color: green'>en ligne!</span>";
 } else {
-    $minecraft_server_connection = "le serveur est <span style='color: red'>hors ligne</span>";
+    $minecraft_vanilla_server_connection = "le serveur est <span style='color: red'>hors ligne</span>";
+}
+
+$minecraft_create_server_connection = @fsockopen("jo.narukami-edition.fr", "999", $errno, $errstr, 0.5);
+if ($minecraft_create_server_connection >= 1) {
+    $minecraft_create_server_connection = "le serveur est <span style='color: green'>en ligne!</span>";
+} else {
+    $minecraft_create_server_connection = "le serveur est <span style='color: red'>hors ligne</span>";
 }
 
 $terraria_server_connection = @fsockopen("jo.narukami-edition.fr", "4649", $errno, $errstr, 0.5);
@@ -13,10 +20,21 @@ if ($terraria_server_connection >= 1) {
     $terraria_server_connection = "le serveur est <span style='color: red'>hors ligne</span>";
 }
 $body = <<<HTML
-<h3>Serveur Minecraft</h3>
-<p>On a un serveur Minecraft, disponible à cette adresse ip: <code>jo.narukami-edition.fr:42957</code>. Actuellement, $minecraft_server_connection</p>
+<h3>Serveurs Minecraft</h3>
+<p>On a deux serveurs Minecraft:
+<ul class="about">
+    <li>Un serveur vanilla 1.19.2, disponible à cette adresse IP: <code>jo.narukami-edition.fr:42957</code>. Actuellement, $minecraft_vanilla_server_connection.</li>
+    <li>Un serveur Create 1.19.2, disponible à cette adresse IP: <code>jo.narukami-edition.fr:999</code>. Actuellement, $minecraft_create_server_connection. <br>Voici les dépendances:
+        <ul class="about">
+            <li><a href="https://fabricmc.net/">Fabric</a></li>
+            <li><a href="https://www.curseforge.com/minecraft/mc-mods/fabric-api/files/4438705">Fabric API</a></li>
+            <li><a href="https://www.curseforge.com/minecraft/mc-mods/create-fabric/files/4537370">Create pour Fabric</a></li>
+        </ul>
+    </li>
+</ul>
+</p>
 <h3>Serveur Terraria</h3>
-<p>On a aussi un serveur Terraria, disponible à cette adresse ip: <code>jo.narukami-edition.fr</code>, et le port <code>4649</code>. Actuellement, $terraria_server_connection</p>
+<p>On a aussi un serveur Terraria, disponible à cette adresse IP: <code>jo.narukami-edition.fr</code>, et le port <code>4649</code>. Actuellement, $terraria_server_connection</p>
 <h4>Si les serveurs sont hors ligne</h4>
-<p>Allez <a href="/index.php?action=wake">ici.</a></p>
+<p>Appuyez <a href="/index.php?action=wake">ici.</a></p>
 HTML;
