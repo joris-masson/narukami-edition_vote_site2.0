044dab1ef929493ff2b0e1b690bd76692928012c
Joris Masson <joris.masson2002@gmail.com>
2023-05-26 13:54:35 +0200

    Complétion du about.php

--- public/php/actions/about.php
+++ public/php/actions/about.php
@@ -4,15 +4,15 @@ $body = <<<HTML
 <h2>À propos</h2>
 <p>Ce site a pour but la gestion des concours photo du serveur Narukami Edition!</p>
 <p>Il est actuellement en développement, mais est accessible au public, afin de tester quelques petites choses. Si vous
-trouvez un bug, veuillez me contacter par discord: <code>JojoAz1605#1980</code>.</p>
-<p>Le code source n'est pas encore disponible, car une bonne partie du code a été repris d'un DM de web de ma licence.
-Je ne veux donc pas prendre le risque de le partager avant les résultats de la première session. Un repo github sera disponible après!</p>
+trouvez un bug, veuillez me contacter par discord: <code>JojoAz1605#1980</code>, ou ouvrir une issue sur le repo Github.</p>
+<p>Le code source est disponible <a href="https://github.com/JojoAz1605/narukami-edition_vote_site2.0">ici</a>.</p>
 
 <h2>Bot discord</h2>
 <p>Un bot discord existe aussi, nommé Kazooha! Vous pouvez obtenir plus de détails <a href="/index.php?action=bot">ici</a>.</p>
 
 <h2>Serveurs</h2>
 <p>On a quelques serveurs(Minecraft et Terraria) de disponibles. Plus d'infos <a href="/index.php?action=server">ici</a>.</p>
+
 <h2>Utilisation des données</h2>
 <p>Ce site possède une sorte de fonction de compte utilisateur en vous connectant via discord, seules ces informations seront utilisées:</p>
 <ul class="about">
@@ -20,9 +20,8 @@ Je ne veux donc pas prendre le risque de le partager avant les résultats de la 
 <li>Votre nom d'utilisateur(ancien ou nouveau format)</li>
 <li>Le hash de votre photo de profil, permettant d'accéder à votre photo de profil</li>
 </ul>
-<br>
-Ces données sont supprimées du serveur après votre déconnexion.
-<br>
+<p>Ces données sont supprimées du serveur après votre déconnexion.</p>
+<p>Le serveur garde aussi en mémoire votre adresse IP afin de se protéger contre le spam, et à des fins de débuggage.</p>
 
 <iframe src="https://discord.com/widget?id=952557533514592286&theme=dark" width="350" height="500" allowtransparency="true" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 HTML;
